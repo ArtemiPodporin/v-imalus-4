05e913f60439676fbe8542d2d7e8bb2d601aeaa1
Artemi Podporin <artemipodporin@gmail.com>
2022-12-13 22:19:21 +0200

    variant 4 ülesanded (1,2)

--- võimalus 4/võimalus_4.py
+++ võimalus 4/võimalus_4.py
@@ -1,9 +1,12 @@
 #13.12.22
 from math import*
 from random import*
-print("ainult negatiivsete summade loendamine")
-P=randint(-10,10)
-print(f"P={P}")
-P=input("kirjuta number")
-if P<0 and P>0:
+a=''
+i=0
+while a !=0:
+    a = int(input("Sisestage number:" ))
+    if a< 0:
+       i += 1
+print("Negatiivsete arvude arv:" + str(i))
+
     
