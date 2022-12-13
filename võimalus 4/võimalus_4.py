22f0b98e7ce52df5a74103ba1c00e2a960ff97d4
opilane <opilane@HP-CZC6297B8C>
2022-12-13 09:52:40 +0200

    Add project files.

---
+++ võimalus 4/võimalus_4.py
@@ -0,0 +1,9 @@
+#13.12.22
+from math import*
+from random import*
+print("ainult negatiivsete summade loendamine")
+P=randint(-10,10)
+print(f"P={P}")
+P=input("kirjuta number")
+if P<0 and P>0:
+    
